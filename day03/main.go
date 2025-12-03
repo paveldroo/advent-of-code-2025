a4fb00fe37bf269c6ce6ec5217617a59606a7f0a
Pavel Druzhinin <paveldroo@gmail.com>
2025-12-03 22:03:54 +0300

    day03 done

--- day03/main.go
+++ day03/main.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/paveldroo/advent-of-code-2025/utils"
 )
 
 func main() {
 	input := utils.MustReadFromFile("input.txt")
-	res := part1(input)
-	// res := part2(input)
+	// res := part1(input)
+	res := part2(input)
 
 	fmt.Printf("TOTAL JOLTS: %d", res)
 }
@@ -19,7 +20,7 @@ func part1(input []string) int {
 	var res int
 	for _, bat := range input {
 
-		q := queue{}
+		q := queuePart1{}
 		for _, val := range bat {
 			valInt, err := strconv.Atoi(string(val))
 			if err != nil {
@@ -38,15 +39,11 @@ func part1(input []string) int {
 	return res
 }
 
-// func part2(input []string) int {
-// 	return 0
-// }
-
-type queue struct {
+type queuePart1 struct {
 	store [2]int
 }
 
-func (q *queue) push(val int) {
+func (q *queuePart1) push(val int) {
 	if q.store[0] == 0 {
 		q.store[0] = q.store[1]
 		q.store[1] = val
@@ -58,4 +55,66 @@ func (q *queue) push(val int) {
 		}
 		q.store[1] = val
 	}
+}
+
+func part2(input []string) int {
+	var res int
+	for _, bat := range input {
+
+		q := queuePart2{}
+		for _, val := range bat {
+			valInt, err := strconv.Atoi(string(val))
+			if err != nil {
+				panic(fmt.Errorf("not valid int string(val): %v", val))
+			}
+			q.push(valInt)
+		}
+
+		res += q.digit()
+	}
+
+	return res
+}
+
+type queuePart2 struct {
+	store []int
+}
+
+func (q *queuePart2) push(val int) {
+	if len(q.store) < 12 {
+		q.store = append(q.store, val)
+		return
+	}
+
+	for i := range q.store {
+		if i == len(q.store)-1 {
+			break
+		}
+
+		if q.store[i] < q.store[i+1] {
+			for j := i; j < len(q.store)-1; j++ {
+				q.store[j] = q.store[j+1]
+			}
+			q.store[11] = val
+			return
+		}
+	}
+
+	if q.store[11] <= val {
+		q.store[11] = val
+	}
+}
+
+func (q *queuePart2) digit() int {
+	var strRes []string
+	for _, val := range q.store {
+		strRes = append(strRes, strconv.Itoa(val))
+	}
+
+	res, err := strconv.Atoi(strings.Join(strRes, ""))
+	if err != nil {
+		panic(fmt.Errorf("not valid int strRes: %v", strRes))
+	}
+
+	return res
 }
--- day03/main_test.go
+++ day03/main_test.go
@@ -46,3 +46,54 @@ func TestPart1(t *testing.T) {
 		})
 	}
 }
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []string
+		want  int
+	}{
+		{
+			name:  "first",
+			input: []string{"987654321111111"},
+			want:  987654321111,
+		},
+		{
+			name:  "second",
+			input: []string{"811111111111119"},
+			want:  811111111119,
+		},
+		{
+			name:  "my second",
+			input: []string{"811111111111919"},
+			want:  811111111919,
+		},
+		{
+			name:  "third",
+			input: []string{"234234234234278"},
+			want:  434234234278,
+		},
+		{
+			name:  "forth",
+			input: []string{"818181911112111"},
+			want:  888911112111,
+		},
+		{
+			name:  "my",
+			input: []string{"888181911112111"},
+			want:  888911112111,
+		},
+		{
+			name:  "full test",
+			input: []string{"987654321111111", "811111111111119", "234234234234278", "818181911112111"},
+			want:  3121910778619,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			res := part2(tc.input)
+			require.Equal(t, tc.want, res)
+		})
+	}
+}
