f6a9e5777c0d001a57d04f2ba4caf33ab7f121da
Pavel Druzhinin <paveldroo@gmail.com>
2025-12-05 16:52:19 +0300

    day05 part1 done

--- day05/main.go
+++ day05/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
+	"strings"
 
 	"github.com/paveldroo/advent-of-code-2025/utils"
 )
@@ -18,7 +21,65 @@ func main() {
 }
 
 func part1(input []string) int {
-	return 0
+	res := make(map[int]struct{})
+	var ranges []string
+	var fresh []string
+	for i, row := range input {
+		if row == "" {
+			ranges = input[:i]
+			fresh = input[i+1:]
+			break
+		}
+	}
+
+	var freshInt []int
+
+	for _, f := range fresh {
+		fInt, err := strconv.Atoi(f)
+		if err != nil {
+			panic(fmt.Sprintf("not valid fresh id: %s", f))
+		}
+		freshInt = append(freshInt, fInt)
+	}
+
+	slices.Sort(freshInt)
+	// freshM := make(map[int]struct{})
+	// for _, f := range fresh {
+	// 	fInt, err := strconv.Atoi(f)
+	// 	if err != nil {
+	// 		panic(fmt.Sprintf("not valid fresh id: %s", f))
+	// 	}
+	// 	freshM[fInt] = struct{}{}
+	// }
+
+	for _, r := range ranges {
+		rr := strings.Split(r, "-")
+		start, err := strconv.Atoi(rr[0])
+		if err != nil {
+			panic(fmt.Sprintf("not valid range start: %s", rr[0]))
+		}
+		end, err := strconv.Atoi(rr[1])
+		if err != nil {
+			panic(fmt.Sprintf("not valid range end: %s", rr[1]))
+		}
+
+		for _, id := range freshInt {
+			if id < start {
+				continue
+			}
+			if id > end {
+				break
+			}
+			res[id] = struct{}{}
+		}
+
+		// for i := start; i <= end; i++ {
+		// 	if _, ok := freshM[i]; ok {
+		// 		res[i] = struct{}{}
+		// 	}
+		// }
+	}
+	return len(res)
 }
 
 // func part2(input []string) int {
