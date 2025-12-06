08da1999d1677a03ab8674422b972f8ba09cd414
Pavel Druzhinin <paveldroo@gmail.com>
2025-12-06 19:52:58 +0300

    day06part1 done

--- day06/main.go
+++ day06/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/paveldroo/advent-of-code-2025/utils"
 )
@@ -18,5 +19,58 @@ func main() {
 }
 
 func part1(input []string) int {
-	return 0
+	var matrix [][]string
+	for _, row := range input {
+		// fmt.Println("INOUT ROW:", row)
+		var curRow []string
+		last := ""
+		// fmt.Println("====================ROW:", row)
+		for _, letter := range row {
+			// fmt.Println("LETTER:", string(letter))
+			// fmt.Println("LAST:", last)
+			if string(letter) == " " {
+				if last != "" {
+					curRow = append(curRow, last)
+					// fmt.Println("CURROW:", curRow)
+					last = ""
+				}
+				continue
+			}
+			last += string(letter)
+		}
+		if last != "" {
+			curRow = append(curRow, last)
+		}
+		matrix = append(matrix, curRow)
+	}
+
+	// fmt.Println("matrix:", matrix)
+
+	res := 0
+
+	for i := range matrix[0] {
+		op := matrix[len(matrix)-1][i]
+		curRes := digit(matrix[0][i])
+		for j := 1; j < len(matrix)-1; j++ {
+			if op == "*" {
+				curRes *= digit(matrix[j][i])
+			} else if op == "+" {
+				curRes += digit(matrix[j][i])
+			} else {
+				panic("not valid operator: " + op)
+			}
+		}
+		res += curRes
+	}
+
+	return res
+}
+
+func digit(input string) int {
+	i, err := strconv.Atoi(input)
+	if err != nil {
+		panic("not valid integer: " + input)
+	}
+
+	return i
 }
